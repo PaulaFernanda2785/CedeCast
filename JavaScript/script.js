bf4f9a53ba76a81317f1b5fdf9ae6025642a6790
PaulaFernanda2785 <paulafernandacorrealima@gmail.com>
2025-09-30 12:39:22 -0300

    ajustes de iframe

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,9 +1,9 @@
 function toggleCheck(link) {
-    // Troca entre caixa vazia (☐) e caixa marcada (✅)
-    if (link.innerHTML.startsWith('&#x2610;') || link.innerHTML.startsWith('☐')) {
-        link.innerHTML = link.innerHTML.replace('&#x2610;', '✅').replace('☐', '✅');
+    // Troca entre caixa vazia (🔘) e caixa marcada (🟠)
+    if (link.innerHTML.startsWith('&#x2610;') || link.innerHTML.startsWith('🔘')) {
+        link.innerHTML = link.innerHTML.replace('&#x2610;', '🟠').replace('🔘', '🟠');
     } else {
-        link.innerHTML = link.innerHTML.replace('✅', '☐');
+        link.innerHTML = link.innerHTML.replace('🟠', '🔘');
     }
     // Evita que o link navegue
     return false;
